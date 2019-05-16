bf4fb018e042af7049b16100c5afd62524854712
Sérgio Mafra <sergio@mafra.io>
2019-05-15 22:54:55 -0300

    Added support to ssh-key creation

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,7 @@
 
 ## VARIABLES
 HOWMANY_CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+USER=$(whoami)
 
 ## ALERTS
 echo "You will be prompted your user password, if you have one."
@@ -16,6 +17,7 @@ echo "Done!"
 
 ## ESSENTIALS
 ## Comment a line if don't want that specific package
+echo "Installing essential packages"
 sudo apt install \
     bmon \
     build-essentials \
@@ -36,16 +38,33 @@ sudo apt install \
     vim \
     wget \
     xz-utils
+echo "Packages Installed"
+echo "Done!"
+
+## SSH key
+echo "Creating a ssh-key"
+mkdir ~/.ssh
+cd ~/.ssh
+ssh-keygen -C "$(USER)-personal-key" -t rsa -b 4096
+echo "ssh-key created..."
+echo "Done!"
 
 ## PYTHON 3.7.2
+echo "Installing Python 3.7.2"
 ## Dependencies
+echo "Installing dependencies first:"
 sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev uuid-dev lzma-dev libffi-dev
+echo "Dependencies installed"
 ## Download
+echo "Downloading Python"
 cd ~/Downloads
 wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+echo "Done!"
+echo "Initiating installation"
 tar xvf Python-3.7.2.tgz
 cd Python-3.7.2
 ./configure --prefix=/usr/local/lib/python3.7 --enable-optimizations
 make -j${HOWMANY_CORES}
 sudo make install
 rm -rf ~/Downloads/Python3.7.2*
+echo "Installed! DOOOONE! finally..."
