b856a6fab5d419808ed913d3f64e9423cec22e57
Sérgio Mafra <sergio@mafra.io>
2019-08-13 11:16:05 -0300

    Changed Python version to 3.7.4

--- prepare.sh
+++ prepare.sh
@@ -2,10 +2,11 @@
 
 ## VARIABLES
 HOWMANY_CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+PYTHON_VERSION=3.7.4
 USER=$(whoami)
 
 ## ALERTS
-echo "You will be prompted your user password, if you have one."
+echo "You will be prompted to type your user's password, if you have one."
 
 ## UPDATE APT REPO
 echo "Initializing APT REPO UPDATE."
@@ -16,7 +17,7 @@ sudo apt upgrade -y
 echo "Done!"
 
 ## ESSENTIALS
-## Comment a line if don't want that specific package
+## Comment a line if you don't want that specific package
 echo "Installing essential packages"
 sudo apt install \
     bmon \
@@ -26,7 +27,9 @@ sudo apt install \
     exfat-fuse \
     exfat-utils \
     ffmpeg \
+    fonts-powerline \
     git \
+    gnome-tweak-tools \
     gparted \
     hping3 \
     htop \
@@ -34,7 +37,10 @@ sudo apt install \
     mosh \
     nmap \
     ntfs-3g \
+    openssh-client \
     openssh-server \
+    powerline \
+    tilix \
     vim \
     wget \
     xz-utils
@@ -45,12 +51,12 @@ echo "Done!"
 echo "Creating a ssh-key"
 mkdir ~/.ssh
 cd ~/.ssh
-ssh-keygen -C "$(USER)-personal-key" -t rsa -b 4096
+ssh-keygen -C "$(USER)-personal-key" -t ed25519 -a 100
 echo "ssh-key created..."
 echo "Done!"
 
-## PYTHON 3.7.2
-echo "Installing Python 3.7.2"
+## PYTHON
+echo "Installing Python ${PYTHON_VERSION}"
 ## Dependencies
 echo "Installing dependencies first:"
 sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev uuid-dev lzma-dev libffi-dev
@@ -58,13 +64,13 @@ echo "Dependencies installed"
 ## Download
 echo "Downloading Python"
 cd ~/Downloads
-wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
 echo "Done!"
 echo "Initiating installation"
-tar xvf Python-3.7.2.tgz
-cd Python-3.7.2
+tar xvf Python-${PYTHON_VERSION}.tgz
+cd Python-${PYTHON_VERSION}
 ./configure --prefix=/usr/local/lib/python3.7 --enable-optimizations
 make -j${HOWMANY_CORES}
 sudo make install
-rm -rf ~/Downloads/Python3.7.2*
+rm -rf ~/Downloads/Python${PYTHON_VERSION}*
 echo "Installed! DOOOONE! finally..."
