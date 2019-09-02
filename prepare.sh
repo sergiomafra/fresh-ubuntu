fae1df10859a920f3b7b53536bfc8381aee99314
Sérgio Mafra <sergio@mafra.io>
2019-09-02 17:55:47 -0300

    I don't know what changed

--- prepare.sh
+++ prepare.sh
@@ -79,7 +79,7 @@ rm -rf ~/Downloads/Python${PYTHON_VERSION}*
 echo "Installed! DOOOONE! finally..."
 
 # Choosing the default terminal emulator (#TeamTilix!)
-echo "Now, you will choose the defualt terminal emulator"
+echo "Now, you will choose the default terminal emulator"
 sudo update-alternatives --config x-terminal-emulator
 echo "Done choosing!"
 
